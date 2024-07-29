855b7a3e08e14a70fc8ad08485dded2aa7579515
Shashi Preetham Mende <shashi.preetham2022@vitstudent.ac.in>
2024-07-29 23:54:11 +0530

    new print statement that prints hello! preetham

--- vit.adda.gitexp.22MIC0136/src/vit/adda/gitexp/pkg22mic0136/VitAddaGitexp22MIC0136.java
+++ vit.adda.gitexp.22MIC0136/src/vit/adda/gitexp/pkg22mic0136/VitAddaGitexp22MIC0136.java
@@ -5,6 +5,7 @@ public class VitAddaGitexp22MIC0136 {
         Student student = new Student("Shashi Preetham Mende", "22MIC0136");
         Professor professor = new Professor("P Sudhakar", "20774", "SCOPE");
         System.out.println("22MIC0136");
+        System.out.println("Hello! Preetham ");
         System.out.println(student.getDetails());
         System.out.println(professor.getDetails());
     }
